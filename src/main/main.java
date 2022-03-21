d959a094fa4ca187e064ff218460941dd9c564f6
AdmiralDogge <96239862+AdmiralDogge@users.noreply.github.com>
2022-03-22 01:13:07 +0200

    Initial commit

---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+
+package main;
+
+import forms.*;
+import java.awt.Point;
+
+public class main {
+    public static void main(String[] args) {
+	frmHub main = new frmHub(new Point(200,50));
+    }
+    
+}
